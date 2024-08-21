27a5035d2a8c97750a1ac8fe760aa4559ddb98ee
お茶 <zoka105105@gmail.com>
2024-08-21 18:01:01 +0800

    Pinia_test_V1

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,13 +1,25 @@
-import { ref, computed, watch } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed, watch } from "vue";
+import { defineStore } from "pinia";
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => {return count.value * 2})
-  const increment = watch()
+export const useCounterStore = defineStore("counter", () => {
+  const count = ref(0);
+  const doubleCount = computed(() => {
+    return count.value * 2;
+  });
   function increment() {
-    count.value++
+    count.value++;
   }
 
-  return { count, doubleCount, increment }
-})
+  return { count, doubleCount, increment };
+});
+
+export const useNameStore = defineStore("name", () => {
+  const age = ref(30);
+  const doubleAge = computed(() => {
+    return age.value * 2;
+  });
+  const increment = () => {
+    age.value++;
+  };
+  return { age, doubleAge, increment };
+});
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from "../components/TheWelcome.vue";
+import { useCounterStore, useNameStore } from "@/stores/counter";
+const counterStore = useCounterStore();
+const nameStore = useNameStore();
+nameStore.name = 10;
 </script>
 
 <template>
   <main>
+    {{ counterStore.count }}
+    {{ nameStore.name }}
     <TheWelcome />
   </main>
 </template>
